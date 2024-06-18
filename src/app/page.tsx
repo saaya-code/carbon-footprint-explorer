5d7ee147e4028ab0b97db7eefdcdf6f08ba5cf8b
saaya-code <boualimohamed74@gmail.com>
2024-06-18 23:01:06 +0100

    added primal loading animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,8 @@ export default function Home() {
   const [results, setResults] = useState<SearchResult[]>([]);
   const [saved, setSaved] = useState<SearchResult[]>([]);
   const [allResults, setAllResults] = useState<SearchResult[]>([]);
+  const [loading, setLoading] = useState(true); 
+  const [loadingData, setLoadingData] = useState(true);
 
   const handleSearch = (query: string) => {
     if (query === '') {
@@ -27,12 +29,16 @@ export default function Home() {
 
   useEffect(() => {
     const fetchResults = async () => {
+      setLoading(true); // Set loading to true before fetching data
       //const apiKey = process.env.NEXT_PUBLIC_CLIMATIQ_API_KEY;
       const res = await fetch(`https://swapi.dev/api/people/`);
       const data = await res.json();
       setAllResults(data.results);
+      setLoading(false); // Set loading to false after data is fetched
+
     };
     const getSavedResults = async () => {
+        setLoadingData(true);
         const res = await fetch('/api/getSaved');
         const data = await res.json();
         let saved = [];
@@ -40,6 +46,7 @@ export default function Home() {
             saved.push(data[i].name);
         }
         setSaved(data);
+        setLoadingData(false);
     }
     fetchResults();
     getSavedResults();
@@ -54,8 +61,25 @@ export default function Home() {
     <div className="min-h-screen bg-ecoBeige text-ecoDarkGreen">
       <div className="container mx-auto p-4">
         <h1 className="text-4xl font-bold text-center mb-4">Carbon Footprint Explorer</h1>
-        <Combobox onSearch={handleSearch} results={results} onSave={handleSave} savedResults={saved} />
-        <SavedResults saved={saved} />
+        {loading ? (
+          <div className="flex justify-center items-center h-screen">
+            <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-400 h-32 w-32"></div>
+          </div>
+        ) : (
+          <>
+            <Combobox onSearch={handleSearch} results={results} onSave={handleSave} savedResults={saved} />
+            {loadingData ? (
+            <>
+            <p>Loading Saved Results...</p>
+            <div className="flex justify-start items-start h-screen">
+            <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-400 h-16 w-16">
+            </div>
+            </div>
+            </>
+            ) : <SavedResults saved={saved} />
+        }
+          </>
+        )}
       </div>
     </div>
   );
